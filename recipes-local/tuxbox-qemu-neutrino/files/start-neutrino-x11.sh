30161c1c684280eda7733148e91cf74c27ed350b
Thilo Graf <dbt@novatux.de>
2026-02-05 19:29:59 +0100

    build (meta-tuxbox): switch to VT before Xorg
    Ensure the QEMU Neutrino launcher switches to VT7 when available and
    pull in kbd for chvt.
    
    Tests:
    - ./cli.py build --machine qemux86-64 --distro tuxbox --distro-type release \
      --target tuxbox-qemu-image

--- recipes-local/tuxbox-qemu-neutrino/files/start-neutrino-x11.sh
+++ recipes-local/tuxbox-qemu-neutrino/files/start-neutrino-x11.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 set -eu
 
-exec /usr/bin/xinit /usr/bin/neutrino -- /usr/bin/Xorg :0 -nolisten tcp -keeptty vt7
+VT=7
+if command -v chvt >/dev/null 2>&1; then
+    chvt "${VT}" || true
+fi
+
+exec /usr/bin/xinit /usr/bin/neutrino -- /usr/bin/Xorg :0 -nolisten tcp -keeptty vt${VT}
